afc91e5a667a89c7c2dff37db95092c775a82dc6
Amir Azhar <amir97azhar@gmail.com>
2022-05-29 16:54:54 +0800

    Implement center component

--- components/sidebar/Sidebar.tsx
+++ components/sidebar/Sidebar.tsx
@@ -4,7 +4,7 @@ const Sidebar = () => {
   const { data: session, status } = useSession()
   console.log(session)
   return (
-    <div className="text-small border-r border-gray-900 p-5 text-gray-500">
+    <div className="text-small h-screen overflow-y-scroll border-r border-gray-900 p-5 text-gray-500 scrollbar-hide">
       <div className="space-y-4">
         <button
           onClick={() => signOut()}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
+// Type
 import type { NextPage } from 'next'
+
+// Component
+import Center from '../components/center/Center'
 import Sidebar from '../components/sidebar/Sidebar'
 
 const Home: NextPage = () => {
   return (
     <div className="h-screen overflow-hidden bg-black">
-      <main>
+      <main className="flex">
         <Sidebar />
-        {/* Center */}
+        <Center />
       </main>
       <div>{/* Player */}</div>
     </div>
