95f0066470a6126b870bf38d7c6ecd56be8e25c9
Amir Azhar <amir97azhar@gmail.com>
2022-06-01 03:08:38 +0800

    useEffect for center

--- components/center/Center.tsx
+++ components/center/Center.tsx
@@ -1,19 +1,37 @@
 // Library
 import { useState, useEffect } from 'react'
+import { useSession } from 'next-auth/react'
+import SpotifyWebApi from 'spotify-web-api-node'
 
 // Helper
 import colorRandomizer from './helpers/colorRandomizer'
 
+// Hook
+import useSpotify from '../../hooks/useSpotify'
+
 // State
-import { useRecoilValue } from 'recoil'
-import { playlistIdState } from '../../atoms/playlistAtom'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { playlistIdState, playlistState } from '../../atoms/playlistAtom'
 
 const Center = () => {
   const playlistId = useRecoilValue(playlistIdState)
   const [color, setColor] = useState('from-indigo-500')
+  const spotifyApi: SpotifyWebApi = useSpotify()
+  const { data: session, status } = useSession()
+  const [playlist, setPlaylist] = useRecoilState<any>(playlistState)
+
   useEffect(() => {
     setColor(colorRandomizer())
   }, [playlistId])
+
+  useEffect(() => {
+    spotifyApi
+      .getPlaylist(playlistId)
+      .then((data) => {
+        setPlaylist(data.body)
+      })
+      .catch((err) => console.log('Oops! Something went wrong!', err))
+  }, [spotifyApi, playlistId])
 
   return (
     <div className="flex-grow">
